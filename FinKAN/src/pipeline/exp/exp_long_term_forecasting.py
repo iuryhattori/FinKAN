d57ae01f087c2ece9eca3c4095e0f3dbd5c72971
Iury Hattori <iuryhattori.prog@gmail.com>
2026-05-05 16:35:51 -0300

    Feat: add exp_long_term_forecasting

---
+++ FinKAN/src/pipeline/exp/exp_long_term_forecasting.py
@@ -0,0 +1,304 @@
+import random
+import optuna
+from data_provider.data_factory import data_provider
+from exp.exp_basic import Exp_Basic
+from utils.tools import EarlyStopping, adjust_learning_rate, AverageMeter
+from layers.KANLinear import KANLinear
+import torch
+import torch.nn as nn
+from torch import optim
+import os
+import time
+import warnings
+import numpy as np
+
+warnings.filterwarnings('ignore')
+
+import psutil
+
+class Exp_Long_Term_Forecast(Exp_Basic):
+    def __init__(self, args):
+        super(Exp_Long_Term_Forecast, self).__init__(args)
+
+    def _build_model(self):
+        model = self.model_dict[self.args.model].Model(self.args).float()
+
+        if self.args.use_multi_gpu and self.args.use_gpu:
+            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        return model
+
+    def _get_data(self, flag):
+        data_set, data_loader = data_provider(self.args, flag)
+        return data_set, data_loader
+
+    def _select_optimizer(self):
+        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+        return model_optim
+
+    def _select_criterion(self):
+        criterion = nn.MSELoss()
+        return criterion
+    
+
+    def vali(self, vali_data, vali_loader, criterion):
+        total_loss = AverageMeter()
+        self.model.eval()
+        with torch.no_grad():
+            for i, batch in enumerate(vali_loader):
+
+                batch_x, batch_y, batch_x_mark, batch_y_mark = batch
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float()
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if self.args.output_attention:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                        else:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                else:
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                outputs = outputs[:, -self.args.pred_len:, :]
+                batch_y = batch_y[:, -self.args.pred_len:, :].to(self.device)
+
+                loss = criterion(outputs, batch_y)
+                
+                total_loss.update(loss.item(), batch_x.size(0))
+        total_loss = total_loss.avg
+        self.model.train()
+        return total_loss
+
+
+    def train(self, setting):
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
+
+        path = os.path.join(self.args.checkpoints, setting)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        time_now = time.time()
+
+        train_steps = len(train_loader) 
+        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+
+        model_optim = self._select_optimizer()
+        criterion = self._select_criterion()
+
+        if self.args.use_amp:
+            scaler = torch.cuda.amp.GradScaler()
+        fix_seed = 2021
+        random.seed(fix_seed)
+        torch.manual_seed(fix_seed)
+        np.random.seed(fix_seed)
+        torch.set_num_threads(6)
+
+        start_time = time.time()
+        process = psutil.Process()
+        start_memory = process.memory_info().rss
+
+
+        for epoch in range(self.args.train_epochs):
+            iter_count = 0
+            train_loss = []
+
+            self.model.train()
+
+            epoch_time = time.time()
+            for i, batch in enumerate(train_loader):
+                iter_count += 1
+                model_optim.zero_grad(set_to_none=True)
+                batch_x, batch_y, batch_x_mark, batch_y_mark, batch_x_aux = batch
+                
+
+                batch_x = batch_x.float().to(self.device)           
+                batch_y = batch_y.float().to(self.device)
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+                if i == 0:
+                    print("batch_x      :", batch_x.shape)
+                    print("batch_x_mark :", batch_x_mark.shape if batch_x_mark is not None else None)
+                    print("batch_x_aux  :", batch_x_aux.shape if batch_x_aux is not None else None)
+
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+
+
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if self.args.output_attention:            
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                        else:
+                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+                        outputs = outputs[:, -self.args.pred_len:, :]
+                        batch_y = batch_y[:, -self.args.pred_len:, :].to(self.device)
+                        loss = criterion(outputs, batch_y)
+
+                else:
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+                    outputs = outputs[:, -self.args.pred_len:, :]
+                    batch_y = batch_y[:, -self.args.pred_len:, :].to(self.device)
+                    loss = criterion(outputs, batch_y)              
+
+                if (i + 1) % 100 == 0:
+                    loss_float = loss.item()
+                    train_loss.append(loss_float)
+                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss_float))
+                    speed = (time.time() - time_now) / iter_count
+                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+                    iter_count = 0
+                    time_now = time.time()
+
+                if self.args.use_amp:
+                    scaler.scale(loss).backward()
+                    scaler.step(model_optim)
+                    scaler.update()
+                else:
+                    loss.backward()
+                    model_optim.step()
+
+            print("Epoch: {} cost time: {}".format(epoch + 1, time.time() - epoch_time))
+
+            train_loss = np.average(train_loss)
+            vali_loss = self.vali(vali_data, vali_loader, criterion)
+            test_loss = self.vali(test_data, test_loader, criterion)
+
+            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
+                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            time_cost = time.time() - epoch_time
+
+            early_stopping(vali_loss, self.model, path)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
+            adjust_learning_rate(model_optim, epoch + 1, self.args)
+                
+       
+        end_time = time.time()
+        end_memory = process.memory_info().rss   
+
+       
+        total_time = end_time - start_time
+        total_memory = end_memory - start_memory  
+
+        print(f"Total Training Time: {total_time:.2f} seconds")
+        print(f"Total Memory Usage: {total_memory / 1024:.2f} MB")
+
+        best_model_path = path + '/' + 'checkpoint.pth'
+        self.model.load_state_dict(torch.load(best_model_path))
+        if not self.args.save_model:
+            import shutil
+            shutil.rmtree(path)
+        return self.model
+
+    def test(self, setting, test=0):
+        fix_seed = 2021
+        random.seed(fix_seed)
+        torch.manual_seed(fix_seed)
+        np.random.seed(fix_seed)
+        torch.set_num_threads(6)
+
+        # Record start time and memory usage
+        start_time = time.time()
+        process = psutil.Process(os.getpid())
+        start_memory = process.memory_info().rss / (1024 * 1024)  # Convert to MB
+
+        test_data, test_loader = self._get_data(flag='test')
+        if test:
+            print('loading model')
+            self.model.load_state_dict(torch.load(os.path.join(self.args.checkpoints + setting, 'checkpoint.pth')))
+
+        mse_loss = nn.MSELoss()
+        mae_loss = nn.L1Loss()
+        mse = AverageMeter()
+        mae = AverageMeter()
+
+        predictions = [] 
+        ground_truth = []  
+
+        total_inference_time = 0  
+        total_memory_used = 0   
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
+                batch_start_time = time.time()
+                process = psutil.Process(os.getpid())
+                batch_start_memory = process.memory_info().rss / (1024 * 1024)
+
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+                # encoder - decoder
+                if self.args.use_amp:
+                    with torch.cuda.amp.autocast():
+                        if self.args.output_attention:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+                        else:
+                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                else:
+                    if self.args.output_attention:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
+
+                    else:
+                        outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
+                outputs = outputs[:, -self.args.pred_len:, :]
+                batch_y = batch_y[:, -self.args.pred_len:, :].to(self.device)
+
+                mse.update(mse_loss(outputs, batch_y).item(), batch_x.size(0))
+                mae.update(mae_loss(outputs, batch_y).item(), batch_x.size(0))
+
+                outputs_np = outputs.detach().cpu().numpy()
+                batch_y_np = batch_y.detach().cpu().numpy()
+
+                outputs_np = test_data.inverse_transform(outputs_np)
+                batch_y_np = test_data.inverse_transform(batch_y_np)
+
+                predictions.append(outputs_np)  
+                ground_truth.append(batch_y_np)  
+
+                batch_end_time = time.time()
+                batch_end_memory = process.memory_info().rss / (1024 * 1024)  
+                total_inference_time += (batch_end_time - batch_start_time)
+                total_memory_used += (batch_end_memory - batch_start_memory)
+
+        print(f"Total Inference Time: {total_inference_time:.2f}S")
+        print(f"Total Memory Usage: {total_memory_used:.2f} MB")
+
+        mse = mse.avg
+        mae = mae.avg
+
+        end_time = time.time()
+        end_memory = process.memory_info().rss / (1024 * 1024) 
+
+        predictions = np.concatenate(predictions, axis=0)
+        ground_truth = np.concatenate(ground_truth, axis=0)
+        print('mse:{}, mae:{}'.format(mse, mae))
+
+        return mse, mae, predictions, ground_truth
