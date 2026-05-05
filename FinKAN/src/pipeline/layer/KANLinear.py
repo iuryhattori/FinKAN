c4560bb5a503e8aa3717c73d65dfd939264ad5d1
Iury Hattori <iuryhattori.prog@gmail.com>
2026-05-05 13:42:10 -0300

    Feat: add kanlinear layer

---
+++ FinKAN/src/pipeline/layer/KANLinear.py
@@ -0,0 +1,44 @@
+﻿import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class KANLinear(nn.Module):
+    def __init__(self, in_features, out_features, 
+                 grid_size, spline_order, grid_range = [-1, 1], base_activation = nn.SiLU):
+        super(KANLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.grid_size = grid_size
+        self.spline_order = spline_order
+        self.register_buffer("grid", grid)
+
+        h = (grid_range[1] - grid_range[0] / grid_size)
+        grid = (torch.arange(-spline_order, grid_size + spline_order + 1) * h + grid_range[0]).expand(in_features, -1).contiguous()
+
+        self.base_weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.spline_weight = nn.Parameter(torch.Tensor(out_features, in_features, grid_size + spline_order))
+        self.base_activation = base_activation()
+
+        def b_splines(self, x):
+            assert x.dim() == 2 and x.size(1) == self.in_features
+            grid = self.grid
+            x = x.unsqueeze(-1)
+            bases = ((x >= grid[:, :-1]) & (x < grid[:, 1:])).to(x.dtype)
+            for k in range(1, self.spline_order + 1):
+                bases = ((x - grid[:, : -(k + 1)]) / (grid[:, k:-1] - grid[:, : -(k + 1)]) * bases[:, :, :-1]) + ((grid[:, k + 1 :] - x) / (grid[:, k + 1 :] - grid[:, 1:(-k)]) * bases[:, :, 1:])
+            assert bases.size() == (x.size(0), self.in_features, self.grid_size + self.spline_order)
+            return bases.contiguous()
+
+        def forward(self, x):
+            original_shape = x.shape
+            if x.dim() == 3:
+                x = x.reshape(-1, x.size(-1))
+            assert x.dim() == 2 and x.size(1) == self.in_features
+            self.acts = self.base_activation(x)
+            base_output = F.linear(self.base_activation(x), self.base_weight)
+            spline_output = F.linear(self.b_splines(x).view(x.size(0), -1), self.scaled_spline_weight.view(self.out_features, -1))
+            output = base_output + spline_output
+            if len(original_shape) == 3:
+                output = output.reshape(original_shape[0], original_shape[1], -1)
+            return output
