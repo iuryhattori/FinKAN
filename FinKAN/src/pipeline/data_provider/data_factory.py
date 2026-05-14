7cd4380e8f702349820c69110967c5b54296415d
Iury Hattori <iuryhattori.prog@gmail.com>
2026-05-14 16:53:12 -0300

    Feat: implement prediction logic in dataloader

--- FinKAN/src/pipeline/data_provider/data_factory.py
+++ FinKAN/src/pipeline/data_provider/data_factory.py
@@ -1,43 +1,61 @@
-from data_provider.data_loader import PETR4_dataset
+from zmq import Flag
+
+from data_provider.data_loader import PETR4_dataset, PETR4_Prediction
 from torch.utils.data import DataLoader
 
 data_dict = {
     'PETR4':  PETR4_dataset,
+    'PETR4_Prediction': PETR4_Prediction
 }
 
-
-def data_provider(args, flag):
-    Data = data_dict[args.data]
-    if flag == 'test':
-        shuffle_flag = False
-        drop_last = False
-        batch_size = args.batch_size
-        freq = args.freq
-    elif flag == 'pred':
+def data_provider(args, flag, prediction_df = None):
+    if flag == 'pred':
         shuffle_flag = False
         drop_last = False
         batch_size = 1
         freq = args.freq
+        data_set = PETR4_Prediction(
+            df = prediction_df,
+            scale = args.scale,
+            freq = args.freq,
+            scalers_path= args.scalers_path,
+            size = [args.seq_len, args.label_len, args.pred_len, args.enc_in]
+        )
+    elif flag == 'test':
+        shuffle_flag = False
+        drop_last = False
+        batch_size = args.batch_size
+        freq = args.freq
+        data_set = PETR4_dataset(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len, args.enc_in],
+            freq=freq,
+            scale=args.scale,
+            scalers_path= args.scalers_path,
+        )
     else:
         shuffle_flag = True
         drop_last = False
-        batch_size = args.batch_size  
+        batch_size = args.batch_size
         freq = args.freq
-
-    data_set = Data(
-        root_path=args.root_path,
-        data_path=args.data_path,
-        flag=flag,
-        size=[args.seq_len, args.label_len, args.pred_len, args.enc_in],
-        freq=freq,
-        scale=args.scale,
+        data_set = PETR4_dataset(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len, args.enc_in],
+            freq=freq,
+            scale=args.scale,
+            scalers_path= args.scalers_path,
         )
-
     print(flag, len(data_set))
     data_loader = DataLoader(
         data_set,
-        batch_size=batch_size,
-        shuffle=shuffle_flag,
+        batch_size = batch_size,
+        shuffle = shuffle_flag,
         num_workers=args.num_workers,
-        drop_last=drop_last)
+        drop_last = drop_last,
+    )
     return data_set, data_loader
+        
--- FinKAN/src/pipeline/data_provider/data_loader.py
+++ FinKAN/src/pipeline/data_provider/data_loader.py
@@ -7,12 +7,13 @@ import pandas as pd
 import torch
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from torch.utils.data import Dataset
+import joblib
 
 warnings.filterwarnings('ignore')
 
 
-class PETR4_dataset(Dataset):
-    def __init__(self, root_path, data_path, flag='train', scale=True, freq='15min', size=None):
+class BasePETR4Dataset(Dataset):
+    def __init__(self, scale=True, freq='15min', size=None, scalers_path='scalers/'):
         if size == None:
             self.seq_len = 24 * 4 * 4
             self.label_len = 24 * 4
@@ -21,20 +22,44 @@ class PETR4_dataset(Dataset):
             self.seq_len = size[0]
             self.label_len = size[1]
             self.pred_len = size[2]
-            
+        
+        self.scale = scale
+        self.freq = freq
+        self.scalers_path = scalers_path
+
+    def inverse_transform(self, data):
+        if not self.scale:
+            return data
+        
+        is_3d = False
+        if len(data.shape) == 3:
+            B, T, C = data.shape
+            data = data.reshape(-1, C)
+            is_3d = True
+        else:
+            C = data.shape[-1]
+        num_features_scaler = self.scaler.n_features_in_
+
+        if C != num_features_scaler:
+            dummy = np.zeros((len(data), num_features_scaler))
+            dummy[:, :C] = data
+            data_denorm = self.scaler.inverse_transform(dummy)[:, :C]
+        else:
+            data_denorm = self.scaler.inverse_transform(data)
+
+        return data_denorm.reshape(B, T, C) if is_3d else data_denorm
+    
+class PETR4_dataset(BasePETR4Dataset):
+    def __init__(self, root_path, data_path, flag='train', scale=True, freq='15min', size=None, scalers_path='scalers/'):
+        super().__init__(scale=scale, freq=freq, size=size, scalers_path=scalers_path)
         assert flag in ['train', 'test', 'val']
         type_map = {'train': 0, 'val': 1, 'test': 2}
-        self.set_type = type_map[flag]  
-        self.seq_len = size[0]
-        self.label_len = size[1]
-        self.pred_len = size[2]
+        self.set_type = type_map[flag]
+        self.scalers_path = scalers_path
         self.root_path = root_path
-        self.scale = scale
         self.data_path = data_path
         self.freq = freq
         self.__read_data__()
-
-    
     def __read_data__(self):
         self.scaler = RobustScaler()
         df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
@@ -58,6 +83,16 @@ class PETR4_dataset(Dataset):
             train_data = df_base[border1s[0] : border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_base.values)
+            if self.set_type == 0:
+                scaler_name = os.path.splitext(self.data_path)[0].replace(os.sep, '_')
+                scaler_path = os.path.join(self.scalers_path, f'{scaler_name}_scaler.pkl')
+                joblib.dump(self.scaler, scaler_path)
+                print(f'[Scaler] Treinado e salvo em: {scaler_path}')
+                print(pd.DataFrame({
+                'Feature' : self.cols_base,
+                'Mediana' : self.scaler.center_,
+                'IQR'     : self.scaler.scale_
+                }).to_string(index=False))
         else:
             data = df_base.values
         data = data.astype(np.float32)
@@ -92,31 +127,49 @@ class PETR4_dataset(Dataset):
         return seq_x, seq_y, seq_x_mark, seq_y_mark
     def __len__(self):
         return len(self.data_x) - self.seq_len - self.pred_len + 1
-    def inverse_transform(self, data):
-        if not self.scale:
-            return data
-            
-        is_3d = False
-        if len(data.shape) == 3:
-            B, T, C = data.shape
-            data = data.reshape(-1, C)
-            is_3d = True
+    
+class PETR4_Prediction(BasePETR4Dataset):
+    def __init__(self, df, scale=True, freq='15min', scalers_path = 'scalers/', size=None):
+        super().__init__(scale=scale, freq=freq, size=size, scalers_path=scalers_path)
+        self.df = df
+        self.__read_data()
+    def __read_data__(self):
+        df = self.df.copy()
+        cols_base = df.columns[1:]
+        self.cols_base = list(cols_base)
+        print(f'Colunas Base : {cols_base}')
+        df_base = df[cols_base]
+        if self.scale:
+            self.scaler = joblib.load(self.scalers_path)
+            data = self.scaler.transform(df_base.values)
         else:
-            C = data.shape[-1]
+            data = df_base.values
+        data = data.astype(np.float32)
 
-        num_features_scaler = self.scaler.n_features_in_
-        
-        if C != num_features_scaler:
-            dummy = np.zeros((len(data), num_features_scaler))
-            dummy[:, :C] = data
-            dummy_denorm = self.scaler.inverse_transform(dummy)
-            data_denorm = dummy_denorm[:, :C]
-        else:
-            data_denorm = self.scaler.inverse_transform(data)
 
-        if is_3d:
-            return data_denorm.reshape(B, T, C)
-        return data_denorm
-            
-    def get_channel_names(self):
-        return self.cols_base
+        df_stamp = df[['date']].copy()
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+
+
+        df_stamp['month'] = df_stamp['date'].dt.month
+        df_stamp['day'] = df_stamp['date'].dt.day
+        df_stamp['weekday'] = df_stamp['date'].dt.weekday
+        df_stamp['hour'] = df_stamp['date'].dt.hour
+        df_stamp['minute'] = df_stamp['date'].dt.minute // 15
+        data_stamp = df_stamp.drop(['date'], axis=1).values
+
+        data_stamp = np.asarray(data_stamp, dtype=np.float32)
+        self.data_x = data
+        self.data_stamp = data_stamp
+
+    def __getitem__(self,index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin : s_end]
+        seq_x_mark = self.data_stamp[s_begin : s_end]
+        return seq_x, seq_x_mark
+    def __len__(self):
+        return len(self.data_x) - self.seq_len + 1
